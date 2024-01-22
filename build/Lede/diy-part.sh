47e2b0b5d6b3ebeffa6027705cb0e21e60da2e4f
hbzmt <58494002+hbzmt@users.noreply.github.com>
2024-01-22 08:27:14 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -99,6 +99,10 @@ profiles.json
 openwrt-x86-64-generic-kernel.bin
 openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz
+01.21-Lede-18.06-6.1.73-x86-64-generic-squashfs-combined.vmdk
+01.21-Lede-18.06-6.1.73-x86-64-generic-squashfs-combined.img.gz
+01.21-Lede-18.06-6.1.73-x86-64-generic-squashfs-combined-efi.img.gz
+ipk.tar.gz
 EOF
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
